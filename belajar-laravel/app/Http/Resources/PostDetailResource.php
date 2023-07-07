f85ebdce77429c6c094978fa0be14d5dc1e85867
492feny <fenysugiantari332@gmail.com>
2023-07-07 17:55:45 +0800

    07-07-2023

---
+++ belajar-laravel/app/Http/Resources/PostDetailResource.php
@@ -0,0 +1,36 @@
+<?php //untuk menampilkan satu data
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'image' => $this->image,
+            'news_content' => $this->news_content,
+            'created_at' => date_format($this->created_at, "Y/m/d H:i:s"),
+            'author' => $this->author,
+            'writer' => $this->whenLoaded('writer'), //penerapan eiger loading
+            'comments' => $this->whenLoaded('comments', function (){
+                return collect($this->comments)->each(function ($comment) {
+                    $comment->commentator;
+                    return $comment;
+                }); 
+            }),
+            'comment_total' => $this->whenLoaded('comments', function () {
+                return $this->comments->count();
+            })
+        ];
+    }
+}
